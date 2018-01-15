43180a6bbd891c9847165d541a99e28d6bc092e7
Luke Przekwas <galactica@Lukes-MacBook-Pro.local>
2018-01-15 12:51:44 -0600

    conditionals to reset i in each array based on length

--- script.js
+++ script.js
@@ -4,24 +4,33 @@ var locationArray = ["Final Destination", "Battlefield", "Yoshi's Story", "Fount
 var itemArray = ["Beam Sword", "Fan", "Fire Thrower", "Hammer", "Home-run Bat", "Mr. Saturn", "Motion-sensor Bomb", "Poison Mushroom", "Poke Ball", "Ray Gun", "Star Rod", "Super Scope", "Banana Peel", "Deku Nut", "Golden Hammer", "Soccer Ball", "Red Shell", "Green Shell", "Boomerang", "Bombchu"];
 
 //for loop to list 100 accusations
-for (var i = 1; i < 101; i++) {
+for (var i = 0; i < 100; i++) {
 
     //makes the h3 tags for each accusation
-    var accusationLink = $("<h3>Accusation " + i + "</h3>");
+    var printedNumber = i + 1;
+    var accusationLink = $("<h3>Accusation " + printedNumber + "</h3>");
 
+    //make a new index using i for a different accusation in each array
+    var friendArrayIndex = friendArray[i];
+    var locationArrayIndex = locationArray[i];
+    var itemArrayIndex = itemArray[i];
+
+    //conditional to get 0-4 repeated for the friends array, 0-9 for location, 0-19 for items
+    if (i >= 5) {
+        friendArrayIndex = friendArray[i % 5];
+        locationArrayIndex = locationArray[i % 10];
+        itemArrayIndex = itemArray[i % 20];
+    }
 
     //test accusation using the function
-    var statement = alertMaker(1, 2, 3);
+    var statement = alertMaker(friendArrayIndex, itemArrayIndex, locationArrayIndex);
     //appends the new accusation to the body
     $(document.body).append(statement);
 
     //function to take 3 parameters and makesd them into a clickable alert message to each accusation
-    function alertMaker(friend, weapon, location) {
+    function alertMaker(friend, item, location) {
         return accusationLink.click(function () {
-            alert("I accuse " + friend + ", with the " + weapon + " in the " + location + "!");
+            alert("I accuse " + friend + ", with the " + item + " on the " + location + "!");
         })
     };
-
-
-
 }
